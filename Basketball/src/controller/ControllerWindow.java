7df28d18c75112f4a218d115770862bb37ef5245
CristianGironza <cristw16@gmail.com>
2018-10-23 23:52:56 -0500

    interface is ready to use

---
+++ Basketball/src/controller/ControllerWindow.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class ControllerWindow implements Initializable {
+	
+	private Button delate;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void deletear(ActionEvent e) {
+		System.out.println("hola");
+	}
+}
